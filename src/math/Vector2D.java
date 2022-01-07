80ce908391f8a67f20fd0500e2de2743456cc567
Ocepris <christian.ocepek@rub.de>
2022-01-07 17:17:38 +0100

    adds: Operations for Vector2D.java

--- src/math/Vector2D.java
+++ src/math/Vector2D.java
@@ -1,34 +1,57 @@
 package math;
 
 public class Vector2D {
-    private float x;
-    private float y;
+    private double x;
+    private double y;
 
     public Vector2D(){
         this.x = 0;
         this.y = 0;
     }
 
-    public Vector2D(float x, float y){
+    public Vector2D(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public float getX() {
+    public Vector2D add(Vector2D direction)
+    {
+        this.x += direction.x;
+        this.y += direction.y;
+        return this;
+    }
+
+    public Vector2D mult(double val)
+    {
+        x *= val;
+        y *= val;
+        return this;
+    }
+
+    public double getLength()
+    {
+        return Math.sqrt(x*x + y*y);
+    }
+
+    public Vector2D normalize()
+    {
+        return this.mult(1 / getLength());
+    }
+
+    public double getX() {
         return x;
     }
 
-    public void setX(float x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public float getY() {
+    public double getY() {
         return y;
     }
 
-    public void setY(float y) {
+    public void setY(double y) {
         this.y = y;
     }
 
-    //TODO add operations to vector
 }
