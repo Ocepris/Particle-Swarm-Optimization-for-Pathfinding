dc75709c0781373ecda0644490f4a3c61ea32d1e
Ocepris <christian.ocepek@rub.de>
2022-01-07 17:18:06 +0100

    implements: Move function for Entities

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,17 +1,45 @@
 package entities;
 
 import math.Vector2D;
+import simulation.EntityController;
 
 public class Entity {
 
     private Vector2D position;
     private Vector2D direction;
 
-    public Entity(){
-        this.position = new Vector2D();
+    private double distance = 2;
+    private Vector2D personalBest;
+
+    public Entity(Vector2D startPos)
+    {
+        this.position = startPos;
+        this.personalBest = startPos;
+        this.direction = new Vector2D(Math.random(), Math.random()).mult(distance);
+
     }
 
+
     public void move(){
+
+        //Calculate direction for Vectors to move into
+        Vector2D dir = new Vector2D(direction.getX() - position.getX(), direction.getY() - position.getY()).normalize().mult(distance);
+        Vector2D pBest = new Vector2D(personalBest.getX() - position.getX(), personalBest.getY() - position.getY()).normalize().mult(distance);
+        Vector2D gBest = new Vector2D(EntityController.GLOBAL_BEST.getX() - position.getX(), EntityController.GLOBAL_BEST.getY() - position.getY()).normalize().mult(distance);
+
+        //Random distance
+        dir.mult(2 * Math.random());
+        pBest.mult(2 * Math.random());
+        gBest.mult(2 * Math.random());
+
+        //Update Position and direction Vector
+        double oldX = position.getX();
+        double oldY = position.getY();
+
+        this.position.add(direction).add(pBest).add(gBest);
+
+        direction.setX(position.getX() - oldX);
+        direction.setY(position.getY() - oldY);
 
     }
 
--- src/simulation/EntityController.java
+++ src/simulation/EntityController.java
@@ -1,6 +1,7 @@
 package simulation;
 
 import entities.Entity;
+import math.Vector2D;
 import visuals.EntityRenderer;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 public class EntityController {
     private ArrayList<Entity> entitiyList;
     private EntityRenderer entRenderer;
+    public static Vector2D GLOBAL_BEST;
 
     public EntityController(EntityRenderer entRenderer){
         this.entRenderer = entRenderer;
